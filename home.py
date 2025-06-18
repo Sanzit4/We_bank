21e5452e4f91c0a94c09d760b48a049b50698953
Sanzit4 <kocknetelhanan@gmail.com>
2025-06-18 01:31:32 +0100

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,10 @@
 import streamlit as st
+st.set_page_config(page_title="Bank App", layout="centered")
 
-st.set_page_config("page_name:'BANK APP'")
+st.title("🏦WE.BANK")
+st.subheader("Welcome to the Future of Banking")
+st.write("Thanks for joining WE.BANK")
+st.write("Your journey to smarter financial living starts here.")
+st.write("Manage your money with confidence—anytime, anywhere.")
+st.write("* Enjoy 24/7 secure online banking")
+st.write("* Get expert financial guidance")
